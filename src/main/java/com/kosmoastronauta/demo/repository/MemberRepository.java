cc8806a1253a14bdc9d84ab3bfe78eaf8f688446
Mateusz Gil <mateusz_gil@outlook.com>
2019-08-09 22:20:39 +0200

    Changed query in MemberRepository to use final strings

--- src/main/java/com/kosmoastronauta/demo/repository/MemberRepository.java
+++ src/main/java/com/kosmoastronauta/demo/repository/MemberRepository.java
@@ -11,6 +11,21 @@ import java.util.List;
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long>
 {
+
+    public static final String TABLE_MEMBER = "member";
+    public static final String RESERVATION_COLUMN_START = "start";
+    public static final String RESERVATION_COLUMN_END = "end";
+    public static final String RESERVATION_COLUMN_MEMBER_ID = "member_id";
+    public static final String TABLE_RESERVATION = "reservation";
+    public static final String TABLE_BOOK = "book";
+    public static final String BOOK_COLUMN_TITLE = "title";
+    public static final String BOOK_COLUMN_AUTHOR = "author";
+    public static final String BOOK_COLUMN_ID = "id";
+    public static final String MEMBER_COLUMN_ID = "id";
+    public static final String MEMBER_COLUMN_NAME = "name";
+    public static final String RESERVATION_COLUMN_BOOK_ID = "book_id";
+    public static final String RESERVATION_COLUMN_RETURNED = "returned";
+
     public List<Member> getMembersByNameEquals(String name);
 
     public List<Member> getMembersByLastNameEquals(String lastName);
@@ -18,15 +33,25 @@ public interface MemberRepository extends CrudRepository<Member, Long>
     public List<Member> getMembersByNameEqualsAndLastNameEquals(String name, String lastName);
 
     @Query(nativeQuery = true, value =
-            "SELECT book.title , book.author," +
-                    " reservation" +
-                    ".start, " +
-                    "reservation.end" +
+            "SELECT " + TABLE_BOOK + "." + BOOK_COLUMN_TITLE + ", " +
+                    TABLE_BOOK+"."+BOOK_COLUMN_AUTHOR+ ", "+
+                    TABLE_RESERVATION + "." + RESERVATION_COLUMN_START + ", "+
+                    TABLE_RESERVATION + "." + RESERVATION_COLUMN_END +
                     " FROM " +
-                    "reservation INNER JOIN " +
-                    "member ON member" + ".id=reservation.member_id INNER JOIN book " + "ON book.id = " +
-                    "reservation" +
-                    ".book_id WHERE member.id=:memberId AND reservation.returned=false")
+                    TABLE_RESERVATION +
+                    " INNER JOIN " + TABLE_MEMBER +
+                    " ON " +
+                    TABLE_MEMBER + "." + MEMBER_COLUMN_ID + "=" + TABLE_RESERVATION + "." +RESERVATION_COLUMN_MEMBER_ID +
+                    " INNER JOIN " +
+                    TABLE_BOOK +
+                    " ON " +
+                    TABLE_BOOK + "." + BOOK_COLUMN_ID +
+                    "=" +
+                    TABLE_RESERVATION + "." + RESERVATION_COLUMN_BOOK_ID +
+                    " WHERE " + TABLE_MEMBER + "." + MEMBER_COLUMN_ID + "=:memberId "+
+                    " AND " +
+                    TABLE_RESERVATION + "." + RESERVATION_COLUMN_RETURNED +
+                    "= false")
     public List<Object[]> getInfoAboutNotReturnedBooksForMemberById(@Param("memberId") long memberId);
 
 }
