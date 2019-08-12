eb58e0e15f43986ec6e21945ead5135fefb82e5e
Mateusz Gil <mateusz_gil@outlook.com>
2019-08-12 10:44:44 +0200

    Added books view, not working yet and change some controllers from annotation @Controller to @RestController

--- src/main/java/com/kosmoastronauta/demo/controllers/BookStatusController.java
+++ src/main/java/com/kosmoastronauta/demo/controllers/BookStatusController.java
@@ -5,12 +5,12 @@ import com.kosmoastronauta.demo.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
-@Controller
+@RestController
 public class BookStatusController
 {
     @Autowired
--- src/main/java/com/kosmoastronauta/demo/controllers/MemberStatusController.java
+++ src/main/java/com/kosmoastronauta/demo/controllers/MemberStatusController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@Controller
+@RestController
 public class MemberStatusController
 {
     @Autowired
