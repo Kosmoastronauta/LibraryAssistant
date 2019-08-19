d5f9433d3a04f93043d8c8d31dfdafd84b455d63
Mateusz Gil <mateusz_gil@outlook.com>
2019-08-19 11:57:17 +0200

    added methods for getting books like in BookRepository

--- src/main/java/com/kosmoastronauta/demo/repository/BookRepository.java
+++ src/main/java/com/kosmoastronauta/demo/repository/BookRepository.java
@@ -8,6 +8,12 @@ import java.util.List;
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long>, BookRepositoryCustom
 {
+
+    public List<Book> getBookByTitleIsLikeAndAndAuthorIsLike(String title, String author);
+
+    public List<Book> getBookByTitleIsLike(String title);
+
+    public List<Book> getBookByAuthorIsLike(String author);
     //  @Query(value ="SELECT * FROM book WHERE book.title=:titleOfBook AND book.free=true",nativeQuery = true)
     public List<Book> getBooksByFreeIsTrueAndTitleEquals(String title);
 
